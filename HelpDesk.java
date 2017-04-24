f0050c086d4434ff9964c4dfc1bd9321e0e6ef7f
ibnullify <ijahan1@stuy.edu>
2017-04-24 06:19:26 -0400

    added comments, and small changes

--- HelpDesk.java
+++ HelpDesk.java
@@ -8,27 +8,34 @@ public class HelpDesk {
     private static int len = 0;
     private static int passed = 0;
 
+    //makes a new queue, list of services offered, and list of solutions to these services
     public static void setup(){
 	_queue = new ArrayPriorityQueue<Ticket>();
 	_serviceList = new ArrayList<String>();
 	_solutionList = new ArrayList<String>();
 	populateServices();
     }
+
+    //added services to the services offered list, and adds solutions to the solutions list
     public static void populateServices(){
 	_serviceList.add("I want to reset password");
 	_solutionList.add("clicking reset password");
 	_serviceList.add("I spilled coffee on my computar");
 	_solutionList.add("wiping up the coffee");
 	_serviceList.add("My computar blew up");
-	_solutionList.add("by buying a new computer");
+	_solutionList.add("buying a new computer");
 	_serviceList.add("I just want to talk");
 	_solutionList.add("giving you a ducky");
     }
+    //customized printing of the services offered
     public static void printServices(){
 	for (int i = 0 ; i < _serviceList.size(); i++){
 	    System.out.println( i + ". " + _serviceList.get(i) );
 	}
     }
+    //starts the process to create a ticket
+    //requires the name, problem, description, solution, and priority of the person and problem being experienced
+    //priority is a random integer value
     public static void makeTicket() {
 	System.out.println("\nWhat's your name, friend?");
 	String user = Keyboard.readString();	
@@ -44,9 +51,10 @@ public class HelpDesk {
 	int priority = (int)(Math.random() * 10);
 	_queue.add(new Ticket(user, description, solution, priority, len));
 	len++;
-	System.out.println("\nGotcha " + user + ", you're on the queue and there are " + _queue.size() + " people on it with you you, please wait"); 
+	System.out.println("\nGotcha " + user + ", you're on the queue and there are " + (_queue.size()-1) + " people on it with you you, please wait"); 
     }
 
+    //removes the minimum value (priority) ticket from the queue, and processes that ticket
     public static void processQueue(){
 	if (_queue.isEmpty()){
 	    System.out.println("//There is nothing to process. Our queue is currently empty.");
@@ -55,7 +63,7 @@ public class HelpDesk {
 	processTicket(_queue.removeMin());
     }
 	
-    
+    //tells the user that submitted the request that it has been taken care of
     public static void processTicket(Ticket ticket){
 	System.out.println("\n\nHey " + ticket.getName() +", luckily we just solved your problem!! If you're wondering, your problem was '' " + ticket.getProblem() + "'' and we did it by " + ticket.getSolution() +".");
     }
@@ -63,6 +71,10 @@ public class HelpDesk {
 	
     public static void main(String[] args) {
         setup();
+	//while loops keeps the HelpDesk constantly running
+	//response 1 allows the user to create a ticket
+	//response 2 allows the user to watch the HelpDesk fix the next problem
+	//response 3 allows the user to see the remaining requests on the queue
 	while (true){//len-passed > 0){
 	    System.out.println("\n\nHello! How can we help you?");
 	    System.out.println("1. I have a problem lolz\n2. I'm just here to watch the HelpDesk run\n3. How's the queue looking?");
@@ -79,8 +91,7 @@ public class HelpDesk {
 	    }
 	}
 	   
-	    //HelpDesk desk = new HelpDesk();
-	    //desk.makeTicket();
+	    
     }
 
     
--- Ticket.java
+++ Ticket.java
@@ -7,6 +7,7 @@ public class Ticket implements Comparable<Ticket>{
     private String name;
     private String solution_descrip;
 
+    //default constructor, mostly for initial testing
     public Ticket(){
 	id = -1;
 	priority = 0;
@@ -16,6 +17,8 @@ public class Ticket implements Comparable<Ticket>{
 	solution_descrip = "";
     }
 
+    //custom constructor
+    //takes values for username, description, solution, priority, and ID
     public Ticket(String user, String Description, String Solution, int Priority, int len){
 	name = user;
 	problem = Description;
@@ -24,19 +27,27 @@ public class Ticket implements Comparable<Ticket>{
 	solution_descrip = Solution;
 	solved = false;
     }
+
+    //accessor to return name
     public String getName(){
 	return name;
     }
+    //accessor to return the problem description
     public String getProblem(){
 	return problem;
     }
+    //accessor to return the solution description
     public String getSolution(){
 	return solution_descrip;
     }
+    //accessor to return the priority
     public int getPrior(){
 	return priority;
     }
 
+    //compares two tickets to determine which one is seen as less
+    //a ticket with a lower priority is seen as a smaller ticket
+    //a ticket with a higher priority is seen as a larger ticket
     public int compareTo(Ticket t){
 	if (t.getPrior() > getPrior())
 	    return -1;
@@ -46,8 +57,9 @@ public class Ticket implements Comparable<Ticket>{
 	    return 0;
     }
 
+    //customized toString prints out the ID, name, and priority associated with a ticket
     public String toString(){
-        return "ID: " + id + ", Name: " + name;
+        return "ID: " + id + ", Name: " + name + ", Priority: " + priority;
 	
     }
 
